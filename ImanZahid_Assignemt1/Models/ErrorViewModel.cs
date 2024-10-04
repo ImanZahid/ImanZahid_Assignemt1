24e770d16c75e4c3d71a691a5e2b32ee8e9b0bc5
ImanZahid <iman.zahid@arksoft.com.tr>
2024-10-04 13:54:50 +0300

    Add project files.

---
+++ ImanZahid_Assignemt1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImanZahid_Assignemt1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
